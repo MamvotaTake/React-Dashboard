3703d4160d4f7285151912780a815cb0603095d7
Takesure Mamvota <tooyoungmamvota@gmail.com>
2023-08-27 16:39:04 +0700

    adding React Query

---
+++ src/services/apiCabins.js
@@ -0,0 +1,32 @@
+import supabase from "./supabase";
+
+export  default async function getCabins() {
+    
+const  { data, error } = await supabase.from('Cabins')
+    .select('*')
+    
+    if (error) {
+        console.error(error)
+        throw new Error("Cabins could not be loaded")
+    }
+
+    return data
+
+}
+
+
+export  async function deleteCabin(id) {
+    
+const { data, error } = await supabase
+  .from('Cabins')
+  .delete()
+        .eq('id', id)
+    
+    if (error) {
+        console.error(error)
+        throw new Error("Cabin could not be deleted")
+    }
+
+    return data;
+}
+
